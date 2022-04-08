829609f52adc2a4c808248d29322883571a36530
Bash-mocart <bashirsuleiman77@gmail.com>
2022-04-08 21:35:38 +0100

    created vpc,subnets

---
+++ subnets.tf
@@ -0,0 +1,18 @@
+resource "aws_subnet" "public" {
+  # The VPC ID.
+  vpc_id = aws_vpc.main.id
+
+  # The CIDR block for the subnet.
+  cidr_block = var.public_subnet_cidr_block
+
+  # The AZ for the subnet.
+  availability_zone = "us-east-1a"
+
+  # Required for EKS. Instances launched into the subnet should be assigned a public IP address.
+  map_public_ip_on_launch = true
+
+  # A map of tags to assign to the resource.
+  tags = {
+    Name                        = "Public subnet"
+  }
+}
