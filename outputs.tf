bf020ef293b414e30d2de38de57b7b53f6af475c
Bash-mocart <bashirsuleiman77@gmail.com>
2022-04-09 00:05:15 +0100

    ouputted resource tags for terratest

--- outputs.tf
+++ outputs.tf
@@ -4,3 +4,17 @@ output "vpc_id" {
   # Setting an output value as sensitive prevents Terraform from showing its value in plan and apply.
   sensitive = false
 }
+
+output "instance_tag" {
+  value       = aws_instance.flugel_instance.tags_all
+  description = "VPC id."
+  # Setting an output value as sensitive prevents Terraform from showing its value in plan and apply.
+  sensitive = false
+}
+
+output "s3_tag" {
+  value       = aws_s3_bucket.flugel_s3.tags_all
+  description = "s3 bucket tags"
+  # Setting an output value as sensitive prevents Terraform from showing its value in plan and apply.
+  sensitive = false
+}
