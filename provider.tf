9387db52cf9f25fbff98387abb9767008643c147
Bash-mocart <bashirsuleiman77@gmail.com>
2022-04-09 12:04:10 +0100

    fixed organization and workspace name for terraform cloud

--- provider.tf
+++ provider.tf
@@ -14,11 +14,11 @@ terraform {
 
   backend "remote" {
 #          The name of your Terraform Cloud organization.
-        organization = "flugel"
+        organization = "flugel-infra"
 #
 #         # The name of the Terraform Cloud workspace to store Terraform state files in.
         workspaces {
-           name = "flugel-infra"
+           name = "flugel"
          }
        }
 }
