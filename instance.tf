1918c91cee5386f4d9c305ae2f856707601ab218
Bash-mocart <bashirsuleiman77@gmail.com>
2022-04-10 11:13:01 +0100

    deleted sec network interface

--- instance.tf
+++ instance.tf
@@ -1,19 +1,7 @@
-resource "aws_network_interface" "instance_network_attach" {
-  subnet_id = aws_subnet.public.id
-
-  tags = {
-    Name = "primary_network_interface"
-  }
-}
 
 resource "aws_instance" "flugel_instance" {
   ami           = var.ami # us-east-1
   instance_type = var.instance_type
-
-  network_interface {
-    network_interface_id = aws_network_interface.instance_network_attach.id
-    device_index         = 0
-  }
 
   tags = {
     Name  = "Flugel"
