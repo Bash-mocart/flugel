1fad7004dae0157282da699772a0aab3f7ddf6da
Bash-mocart <bashirsuleiman77@gmail.com>
2022-04-10 10:57:24 +0100

    added comments

--- infra_test.go
+++ infra_test.go
@@ -16,9 +16,10 @@ func TestTerraformInfraTest(t *testing.T) {
 	defer terraform.Destroy(t, terraformOptions)
 
 	terraform.InitAndApply(t, terraformOptions)
-
+	// saving terraform outputs in variables
 	s3_output := terraform.Output(t, terraformOptions, "s3_tag")
 	instance_tag_output := terraform.Output(t, terraformOptions, "instance_tag")
+	// checking the resources are properly tagged
 	assert.Equal(t, "map[Name:Flugel Owner:InfraTeam]", s3_output)
 	assert.Equal(t, "map[Name:Flugel Owner:InfraTeam]", instance_tag_output)
 }
