214885e65b3f0658bc3d8e43c5ac6b4389a719f7
Bash-mocart <bashirsuleiman77@gmail.com>
2022-04-10 11:26:55 +0100

    deleted vpc ip output

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,3 @@
-output "vpc_id" {
-  value       = aws_vpc.main.id
-  description = "VPC id."
-  # Setting an output value as sensitive prevents Terraform from showing its value in plan and apply.
-  sensitive = false
-}
-
 output "instance_tag" {
   value       = aws_instance.flugel_instance.tags
   description = "VPC id."
