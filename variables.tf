46cc96dd3743a74b7a5025c3f881e66665a28232
Bash-mocart <bashirsuleiman77@gmail.com>
2022-04-10 11:44:25 +0100

    attached mistakenly deleted ENI

--- variables.tf
+++ variables.tf
@@ -1,3 +1,12 @@
+variable "vpc_cidr_block" {
+  type    = string
+  default = "10.0.0.0/16"
+}
+variable "public_subnet_cidr_block" {
+  type    = string
+  default = "10.0.1.0/24"
+}
+
 variable "s3_bucket" {
   type    = string
   default = "bashox1234567890"
