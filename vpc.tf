72edf2e5e7a8bf15279ce37e9a349ca7dda5b9d3
Bash-mocart <bashirsuleiman77@gmail.com>
2022-04-10 17:32:24 +0100

    configured vpc for eks

--- vpc.tf
+++ vpc.tf
@@ -1,6 +1,26 @@
 resource "aws_vpc" "my_vpc" {
   cidr_block = var.vpc_cidr_block
 
+
+  # Makes your instances shared on the host.
+  instance_tenancy = "default"
+
+  # Required for EKS. Enable/disable DNS support in the VPC.
+  enable_dns_support = true
+
+  # Required for EKS. Enable/disable DNS hostnames in the VPC.
+  enable_dns_hostnames = true
+
+  # Enable/disable ClassicLink for the VPC.
+  enable_classiclink = false
+
+  # Enable/disable ClassicLink DNS Support for the VPC.
+  enable_classiclink_dns_support = false
+
+  # Requests an Amazon-provided IPv6 CIDR block with a /56 prefix length for the VPC.
+  assign_generated_ipv6_cidr_block = false
+
+
   tags = {
     Name = "vpc"
   }
@@ -11,7 +31,13 @@ resource "aws_subnet" "my_subnet" {
   cidr_block        = var.public_subnet_cidr_block
   availability_zone = "us-east-1a"
 
+  # Required for EKS. Instances launched into the subnet should be assigned a public IP address.
+  map_public_ip_on_launch = true
+
+  # A map of tags to assign to the resource.
   tags = {
-    Name = "subnet"
+    Name                        = "public-eu-west-3b"
+    "kubernetes.io/cluster/eks" = "shared"
+    "kubernetes.io/role/elb"    = 1
   }
 }
